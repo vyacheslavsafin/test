c75749183967a6a4ac6e9a3273b85d0e126e2876
vyacheslavsafin <safin.vr@mail.ru>
2023-06-08 20:13:54 +0300

    qwewrt

---
+++ funcs.py
@@ -0,0 +1,7 @@
+def sum_func(a, b):
+    total = a + b
+    return total
+
+def subtract(a, b):
+    result = a - b
+    return result
---
+++ main.py
@@ -0,0 +1,4 @@
+from funcs import sum_func, subtract
+
+print(sum_func(1, 2))
+print(subtract(2,3))
